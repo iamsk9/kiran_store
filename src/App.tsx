16397f0801566e6b88c8b9d2048607050acd9169
A Sai Krishna <athkurisaikrishna@gmail.com>
2025-11-30 22:44:55 +0530

    Branch commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <h1>Kiran Store</h1>
+        <h1>Sai Krishna General Store</h1>
         <p>Discover amazing products at great prices</p>
       </header>
       <main className="app-main">
